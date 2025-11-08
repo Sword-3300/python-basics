d9e37cd8acb74e78e175ea69302c1d9f5d1be31d
Sword <verigo.dmitry@gmail.com>
2025-11-08 16:59:24 +0500

    3&6-2,3

--- 3&6.py
+++ 3&6.py
@@ -22,3 +22,46 @@ def first():
             GPUs_new.remove(GPU)
 
     print("Новый список видеокарт",', '.join(str(GPU_new) for GPU_new in GPUs_new))
+
+def second():
+    films = ["Крепкий орешек",
+             "Назад в будущее",
+             "Таксист",
+             "Леон",
+             "Богемская рапсодия",
+             "Город грехов",
+             "Мементо",
+             "Отступники",
+             "Деревня"]
+
+    favourite_films = []
+
+    number_of_films = int(input("Сколько фильмов хотите добавить? "))
+
+    for _ in range(number_of_films):
+        new_film = input("Введите название фильма: ")
+        if new_film in films:
+            favourite_films.append(new_film)
+        else:
+            print(f"Ошибка: фильма {new_film} у нас нет :(")
+
+    print("Ваш список любимых фильмов:", ', '.join(favourite_films))
+
+def third():
+    def shifting(initial_list, shift):
+        final_list = list(initial_list)
+        final_list_temp = list(final_list)
+
+        for _ in range(shift):
+            for i, v in enumerate(final_list):
+                if i < len(final_list) - 1:
+                    final_list_temp[i + 1] = final_list[i]
+                else:
+                    final_list_temp[-i - 1] = final_list[i]
+            final_list = list(final_list_temp)
+
+        return final_list
+
+    initial_list = input("Изначальный список: ").split(", ") # Формат: a, b, c, d, ...
+    shift = int(input("Сдвиг: "))
+    print("Сдвинуый список:", ', '.join(shifting(initial_list, shift)))
